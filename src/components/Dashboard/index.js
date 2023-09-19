ec0e2cc4f430437ea8615416d798784118f54fe6
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-09-19 17:07:25 -0600

    Finished dashboard flow

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -5,24 +5,25 @@ import phoneImg from "@assets/phoneImg.png";
 
 const Dashboard = () => {
   return (
-    <main className="bg-light flex flex-wrap items-center justify-center md:p-4 sm:p-3">
+    <main className="bg-light flex flex-wrap items-center justify-around md:p-4 sm:p-3">
       <div>
-        <Image src={phoneImg} alt="phoneImg" width={500} height={500} />
+        <Image src={phoneImg} alt="phoneImg" width={550} height={550} />
       </div>
       <div className="flex flex-col items-center md:pt-4 sm:pt-4">
-        <h2 className="text-red font-medium text-5xl md:text-4xl sm:text-4xl">
-          Crea tu producto
+        <h2 className="text-green font-medium text-5xl md:text-6xl sm:text-4xl">
+          Conocenos
         </h2>
-        <p className="text-dark font-medium text-3xl text-justify md:text-2xl sm:text-2xl md:pb-3 sm:pb-3">
-          Registrate y organiza de <br />
-          manera profesional <br />
-          tu inventario.
+        <p className="text-dark font-normal text-3xl text-justify md:text-3xl sm:text-3xl md:pb-3 sm:pb-3">
+          Estamos afiliados a <br />
+          muchos bancos y con la <br />
+          app puedes tener todos <br />
+          en uno.
         </p>
         <Link
           href="/Banks"
-          className="text-red  bg-mint font-normal text-base rounded px-4 py-2 mt-4 md:w-40 md:py-3 sm:w-40 sm:py-3 hover:bg-red hover:border-light hover:text-ligth"
+          className="text-light bg-brown font-normal text-base rounded px-4 py-2 mt-4 md:w-40 md:py-3 sm:w-40 sm:py-3 hover:bg-red hover:text-light"
         >
-          Registrate y administra tu inventario
+          Bancos afiliados
         </Link>
       </div>
     </main>
