a0275631a8ce91560d2b7afa398794f108b924d9
Miriam Nava <71458522+MiriamNM@users.noreply.github.com>
2023-09-19 20:26:53 -0600

    finished Dashboard Banks tests

--- src/components/Header/header.test.js
+++ src/components/Header/header.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "@components/Header";
 import { useRouter } from "next/router";
@@ -12,7 +12,7 @@ describe("Header", () => {
   it("renders the header content correctly", () => {
     useRouter.mockReturnValue({ pathname: "/" });
 
-    render(<Header />);
+    render(<Header onChange={() => {}} />);
 
     expect(screen.getByAltText("logo")).toBeInTheDocument();
 
@@ -20,14 +20,28 @@ describe("Header", () => {
     expect(screen.getByRole("link")).toHaveAttribute("href", "/Banks");
   });
 
-  it("renders the header content correctly when on the Banks page", () => {
+  it("renders the search input when on the Banks page", () => {
     useRouter.mockReturnValue({ pathname: "/Banks" });
 
-    render(<Header />);
+    render(<Header onChange={() => {}} />);
 
     expect(screen.getByAltText("logo")).toBeInTheDocument();
 
-    expect(screen.getByText("Home")).toBeInTheDocument();
-    expect(screen.getByRole("link")).toHaveAttribute("href", "/");
+    expect(
+      screen.getByPlaceholderText("Busca el producto"),
+    ).toBeInTheDocument();
+  });
+
+  it("triggers the onChange prop when typing in the search input", () => {
+    useRouter.mockReturnValue({ pathname: "/Banks" });
+
+    const mockOnChange = jest.fn();
+
+    render(<Header onChange={mockOnChange} />);
+
+    const searchInput = screen.getByPlaceholderText("Busca el producto");
+    fireEvent.change(searchInput, { target: { value: "term" } });
+
+    expect(mockOnChange).toHaveBeenCalledWith("term");
   });
 });
--- src/views/Dashboard/dashboard.test.js
+++ src/views/Dashboard/dashboard.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Dashboard from "views/Dashboard";
+import Dashboard from "@views/Dashboard";
 
 describe("Dashboard", () => {
   it("renders the dashboard content correctly", () => {
